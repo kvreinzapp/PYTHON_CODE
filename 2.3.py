ee56ea0c772475e877dd43614164ae3979da5904
klaus <18848113328@163.com>
2022-06-21 11:38:07 +0800

    py4e

---
+++ 2.3.py
@@ -0,0 +1,8 @@
+# This first line is provided for you
+
+hrs = input("Enter Hours:")
+#hrs = float(hrs)
+rates = input("Enter Rates:")
+#rates = float(rates)
+pays = float(hrs*rates)
+print("Pay:", pays)
