ffd3f84d45dfcbe702a9babfd5e7109d18ed0b99
klaus <18848113328@163.com>
2022-06-24 15:55:27 +0800

    Chapter3 is Done, about 3 hours to finish it

---
+++ Python_Crash_Course/Chapter3:Introducing_List/Avoiding_index_errors/How_to_avoid.py
@@ -0,0 +1,5 @@
+# If an index error occurs and you can’t figure out how to resolve it, try printing your
+# list or just printing the length of your list. Your list might look much different than
+# you thought it did, especially if it has been managed dynamically by your program.
+# Seeing the actual list, or the exact number of items in your list, can help you sort out
+# such logical errors.
---
+++ Python_Crash_Course/Chapter3:Introducing_List/everyFuction_sumOFchap3.py
@@ -0,0 +1,17 @@
+# method : lower upper title append insert pop remove reverse sort
+# function : len sorted
+# statemnt : del
+# other thing : f"" [-1]
+
+# lower upper title : nothing to tell, basically use
+# .append('str') : add in the end
+# .insert(position,'str') : insert in a postion, PA the change of postion after last insert
+# .pop('str') : a unique method, has value itself [reuse by store in a variable]
+# .remove('str') : [permannently] [reuse by store in the fist] [just for first occurrence]
+# .reverse() : [permannently] [double use to restore]
+# .sort() : [permannently] [(reverse=True) to alphabetically reverse]
+
+# len(variable) : show the length [no off to one errors]
+# sorted(variable) : [temporarliy] [(variable, reverse=True) to reverse]
+
+# del list[position]
