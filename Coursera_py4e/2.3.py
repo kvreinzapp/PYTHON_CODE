f5ee43d3974ace5d7e709a168db1f2f0940aae34
kvreinzapp <klausvonreinherzapp@gmail.com>
2024-08-15 16:58:48 +0800

    Week4 add the practice code

--- Coursera_py4e/2.3.py
+++
@@ -1,5 +0,0 @@
-# This first line is provided for you
-hrs = input("Enter Hours:")
-rates = input("Enter Rates:")
-pays = float(hrs)*float(rates)
-print("Pay:", pays)
---
+++ Coursera_py4e/US_European_floor.py
@@ -0,0 +1,3 @@
+us_floor = input('Your US floor is:')
+european_floor = int(us_floor) - 1
+print('Your EUROPEAN floor is', european_floor)
