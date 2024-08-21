3047586244f7cdd66ab88891f2882f28a8d51c38
kvreinzapp <klausvonreinherzapp@gmail.com>
2024-08-21 16:42:20 +0800

    disc01 Q3 is done

---
+++ CS61A_24summer/Disc/disc01/03_isPrime.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    if (n == 1):
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
