59d2a90e5076d44e4ea879e0baead907e52a772c
kvreinzapp <klausvonreinherzapp@gmail.com>
2025-01-08 13:59:46 +0800

    exercise 23, just index the lists, embbed lists

---
+++ lpythw/ex23.py
@@ -0,0 +1,14 @@
+fruit = [['Apple', 12, 'AAA'], ['Organges', 1, 'B'], ['Pears', 2, 'A'],
+         ['Grapes', 14, 'UR']]
+
+cars = [['Cadillac', ['Black', 'Big', 34500]],
+        ['Corvette', ['Red', 'Little', 1000000]],
+        ['Ford', ['Blue', 'Medium', 1234]], ['BMW', ['White', 'Baby', 7890]]]
+
+languages = [
+    ['Python', ['Slow', ['Terrible', 'Mush']]],
+    ['JavaSCript', ['Moderate', ['Alright', 'Bizarre']]],
+    ['Perl6', ['Moderate', ['Fun', 'Weird']]],
+    ['C', ['Fast', ['Annoying', 'Dangerous']]],
+    ['Forth', ['Fast', ['Fun', 'Difficult']]],
+]
