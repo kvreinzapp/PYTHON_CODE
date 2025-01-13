e4ae90ec9d1a9fce83cc0baf04a5de5e735d5c2b
kvreinzapp <klausvonreinherzapp@gmail.com>
2025-01-13 10:59:09 +0800

    Just some randome use of dicitonary, no need to type them all

---
+++ lpythw/ex43.py
@@ -0,0 +1,16 @@
+states = {
+    'Oregon': 'OR',
+    'Florida': 'Fl',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+cities = {"CA": "San Francisco", "MI": "Detroit", 'FL': "Jacksonville"}
+
+cities['NY'] = "New York"
+cities['OR'] = "Portland"
+
+print('-' * 10)
+print("NY states has", cities["NY"])
+print("NY states has", cities["NY"])
