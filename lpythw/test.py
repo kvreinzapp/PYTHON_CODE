87e9941b4aeaca268cc374ca877b6cad447922d4
kvreinzapp <klausvonreinherzapp@gmail.com>
2025-01-13 15:54:04 +0800

    add test.py that could spin the character

--- lpythw/test.py
+++ lpythw/test.py
@@ -1,1 +1,20 @@
-print(print('he'))
+import sys
+import time
+
+# Characters for the spinner
+spinner = ['|', '/', '-', '\\', '-']
+
+
+# Spinner function
+def spin(duration=5, interval=0.2):
+    end_time = time.time() + duration  # Run spinner for `duration` seconds
+    while time.time() < end_time:
+        for frame in spinner:
+            # Print the spinner character
+            sys.stdout.write(f'\r{frame} ')
+            sys.stdout.flush()  # Force the output to display immediately
+            time.sleep(interval)  # Wait for `interval` seconds
+
+
+# Run the spinner
+spin()
